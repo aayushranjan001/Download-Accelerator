34dfe820b64757936bc45ddaa80e764d99a784cb
Aayush Ranjan <aayushranjan001@gmail.com>
2025-02-17 05:45:34 +0530

    Added single threaded download

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
 	url := flag.String("url", "", "Download URL")
+	fileName := flag.String("file", "file", "Location where file to be saved")
 
 	flag.Parse()
 
@@ -15,5 +19,31 @@ func main() {
 		return
 	}
 
-	fmt.Println("Received URL:",*url)
+	fmt.Println("Received URL:", *url)
+
+	err := DownloadFile(fileName, url)
+	if err != nil {
+		fmt.Println("Error while downloading file", err)
+	} else {
+		fmt.Println("Download completed:", fileName)
+	}
+}
+
+func DownloadFile(fileName, url *string) error {
+	resp, err := http.Get(*url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(*fileName)
+	if err != nil {
+		fmt.Print("Error: Error while creating file")
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+
 }
